b1f7518d1f5892c707e238ae8920c3f1abbcfacb
Chris Gullickson <cgullickson5@gmail.com>
2017-12-07 22:23:28 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,16 @@
 class Backer
   attr_accessor :name
 
-  @@backed_projects = []
-
   def initialize (name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
     new_project = Project.new(project)
     @@backed_projects << new_project
   end
+
+  
 
 end
